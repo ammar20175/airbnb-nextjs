8a000562369e6a6133b19dff5438bcac2747555d
ammarahmad <ammar.ahmad6410@gmail.com>
2023-10-19 15:20:15 +0500

    project setup plus header component done.

---
+++ pages/index.js
@@ -0,0 +1,16 @@
+import Header from "@/components/Header";
+import Head from "next/head";
+
+export default function Home() {
+	return (
+		<div className="">
+			<Head>
+				<title>Airbnb</title>
+			</Head>
+
+			{/* Header */}
+			<Header />
+			{/* Banner */}
+		</div>
+	);
+}
